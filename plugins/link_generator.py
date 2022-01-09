052c3c50acd71cadebc9cb8fbab449a308f71798
AppleBotz <95849390+AppleBotz@users.noreply.github.com>
2022-01-10 00:54:56 +0700

    Update :")

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -54,7 +54,7 @@ async def batch(client: Client, message: Message):
         [
             [
                 InlineKeyboardButton(
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}",
+                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"),
                 InlineKeyboardButton(
                     "📦 Batch", url=f"{link}"
                 )
@@ -95,7 +95,7 @@ async def link_generator(client: Client, message: Message):
         [
             [
                 InlineKeyboardButton(
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}",
+                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"),
                 InlineKeyboardButton(
                     "🗂 Genlink", url=f"{link}"
                 )
