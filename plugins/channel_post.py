7d73ce8907f81783db48abec02ae1e28d8654387
AppleBotz <95849390+AppleBotz@users.noreply.github.com>
2022-01-10 02:36:47 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -18,7 +18,7 @@ from helper_func import encode
     )
 )
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("🕷", quote=True)
+    reply_text = await message.reply_text("😈", quote=True)
     try:
         post_message = await message.copy(
             chat_id=client.db_channel.id, disable_notification=True
@@ -48,7 +48,7 @@ async def channel_post(client: Client, message: Message):
     )
 
     await reply_text.edit(
-        f"<code>✅Succses Create Link...</code>\n\nHere is your link :\n{link}",
+        f"<code>✅ Succses Create Link...</code>\n\nHere is your link :\n{link}",
         reply_markup=reply_markup,
         disable_web_page_preview=True,
     )
